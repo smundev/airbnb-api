d4c6766ba9cfc1ae539c8d9d659e053e67d686e0
Samuel Mundo <samuelmundo@gmail.com>
2023-06-01 20:12:54 -0600

    Adding json-server-auth

--- api/server.js
+++ api/server.js
@@ -1,14 +1,19 @@
 const jsonServer = require("json-server");
+const auth = require("json-server-auth");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+server.db = router.db;
 
 server.use(middlewares);
 server.use(
   jsonServer.rewriter({
     "/api/*": "/$1",
+    users: 600,
+    messages: 640,
   })
 );
+server.use(auth);
 server.use(router);
 server.listen(3000, () => {
   console.log("JSON Server is running");
