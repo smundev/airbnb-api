af3cef67e0ac7a19cbe991d2891479a90f18180f
Samuel Mundo <samuelmundo@gmail.com>
2023-06-02 09:45:11 -0600

    fix/write issue

--- api/server.js
+++ api/server.js
@@ -1,10 +1,7 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
-const path = require("path");
 const server = jsonServer.create();
-const fs = require("fs");
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
-const router = jsonServer.router(db);
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 server.db = router.db;
 
